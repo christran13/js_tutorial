23af412838d4b76bd0c6f114b0a5692afc09d1bf
Christopher Tran <chris.tran1113@gmailcom>
2018-05-31 19:43:18 -0400

    second

---
+++ day.js
@@ -0,0 +1,7 @@
+function dayOfTheWeek(date) {
+	const daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+	return daysOfTheWeek[date.getDay()];
+}
+
+
+
---
+++ palindrome.js
@@ -0,0 +1,9 @@
+// reverses a string.
+function reverse(string) {
+	return Array.from(string).reverse().join("");
+}
+
+function palindrome(string) {
+	let processedContent = string.toLowerCase();
+	return processedContent === reverse(processedContent);
+}
